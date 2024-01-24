2a526030fc272a661bfe5c5f4cf1e2c2dac3ba3e
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2024-01-24 21:41:58 +0500

    App.jsx

--- Code/src/App.jsx
+++ Code/src/App.jsx
@@ -1,32 +1,12 @@
 import { useState } from 'react';
-import Header from './components/Header/Header';
-import MainContent from './components/MainContent/MainContent';
-import Header1 from './components2/Header1';
-import Footer1 from './components2/Footer1';
-import MainContent1 from './components2/MainContent1';
-import 'bootstrap/dist/css/bootstrap.css';
-import Practice from './Practice/Practice';
-import Memegenerator from './Practice/Memegenerator';
+//import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Tenzies from './Tenzies/Tenzies';
 
 function App() {
-  // return (
-  //   <div className="box">
-  //     <Header />
-  //     <MainContent />
-  //   </div>
-  // );
-
+ 
   return (
     <div className="box">
-      {/* <Memegenerator /> */}
-      {/* <Practice /> */}
-      {/* <Header1 />
-      <MainContent1 />
-      <Footer1 /> */}
-      {/* <Header /> */}
-      {/* <MainContent/> */}
       <Tenzies />
     </div>
   );
